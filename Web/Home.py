b42519a79b0ef6e28b5b6debfe139be66b25531e
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2022-07-09 20:47:05 +0700

    Pogress

---
+++ Web/Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Regresi dan Klasifikasi Data ISPU",
+    page_icon="📈",
+)
+
+st.write("Selamat datang")
+
+st.sidebar.success("Pilih fitur.")
+
+st.markdown(
+    """
+    Aplikasi ini dibuat dengan menggunakan streamlit untuk melakukan visualisasi regresi dan klasifikasi Data ISPU.
+"""
+)

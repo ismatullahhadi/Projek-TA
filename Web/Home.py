a1a3027f38edd8b95c3c2468dc9c0315e89e6f07
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2022-07-18 15:48:39 +0700

    Web Progress

--- Web/Home.py
+++ Web/Home.py
@@ -1,16 +1,111 @@
 import streamlit as st
+from PIL import Image
+from xarray import align
 
 st.set_page_config(
-    page_title="Regresi dan Klasifikasi Data ISPU",
-    page_icon="📈",
+    page_title="Halaman Utama",
+    page_icon="🏠",
 )
 
-st.write("Selamat datang")
+st.title("Selamat datang")
 
-st.sidebar.success("Pilih fitur.")
+st.sidebar.info("Pilih fitur.")
 
 st.markdown(
     """
+    <div style='text-align: justify;'>
     Aplikasi ini dibuat dengan menggunakan streamlit untuk melakukan visualisasi regresi dan klasifikasi Data ISPU.
-"""
+    
+    Data ISPU juga bisa diakses di [halaman resmi.](https://ispu.menlhk.go.id/map.html)
+    </div>
+""", unsafe_allow_html=True
+)
+
+st.header("Apa itu ISPU?")
+
+st.markdown(
+    """
+        <div style='text-align: justify;'>
+        Menurut Peraturan Menteri Lingkungan Hidup dan Kehutanan Nomor P.14/Menlhk/Setjen/Kum.1/7/2020 tentang Indeks Standar Pencemar Udara, 
+        <b>ISPU</b> merupakan angka tanpa satuan yang menggambarkan kondisi kualitas udara ambien di lokasi tertentu, yang didasarkan pada dampak terhadap 
+        kesehatan manusia, nilai estetik, dan makhluk hidup lainnya. Adapun parameter ISPU meliputi Hidrokarbon (HC), Karbon monoksida (CO), Sulfur dioksida (SO2), 
+        Nitrogen dioksida (NO2), Ozon (O3), dan Partikulat (PM10 dan PM2,5).
+        </div>
+    """, unsafe_allow_html=True
+)
+
+tabel_kategori_ispu = Image.open('./images/Tabel_kategori_indeks_ISPU.png')
+
+col1, col2, col3 = st.columns([1,6,1])
+
+with col1:
+    st.write("")
+
+with col2:
+    st.image(tabel_kategori_ispu, caption='Tabel Kategori Indeks ISPU')
+
+with col3:
+    st.write("")
+
+st.markdown(
+    """
+        <div style='text-align: justify;'>
+        Hasil perhitungan ISPU parameter PM2.5 disampaikan kepada publik tiap jam selama 24 jam. Sedangkan hasil perhitungan ISPU parameter PM10, NO2, SO2, CO, O3, 
+        dan HC disampaikan kepada publik paling sedikit 2 (dua) kali dalam 1 (satu) hari pada pukul 09.00 dan 15.00. Tabel konversi nilai konsentrasi parameter ISPU 
+        dan cara perhitungan sebagai berikut:
+        </div>
+    """, unsafe_allow_html=True
+)
+
+konversi_nilai_konsentrasi = Image.open('./images/Konversi_nilai_konsentrasi.png')
+
+col1, col2, col3 = st.columns([1,6,1])
+
+with col1:
+    st.write("")
+
+with col2:
+    st.image(konversi_nilai_konsentrasi, caption='Konversi Nilai Konsentrasi')
+
+with col3:
+    st.write("")
+
+st.markdown(
+    """
+        <div style='text-align: justify;'>
+        Perhitungan ISPU dilakukan berdasarkan nilai ISPU batas atas, ISPU batas bawah, ambien batas atas, ambien batas bawah, dan konsentrasi ambien hasil pengukuran. 
+        Persamaan matematika perhitungan ISPU sebagai berikut:
+        </div>
+    """, unsafe_allow_html=True
+)
+
+rumus_ISPU = Image.open('./images/rumus_ISPU.png')
+
+col1, col2, col3 = st.columns([1,1,1])
+
+with col1:
+    st.write("")
+
+with col2:
+    st.image(rumus_ISPU)
+
+with col3:
+    st.write("")
+
+st.markdown(
+    """
+        Keterangan:
+
+        I = ISPU terhitung
+        
+        Ia = ISPU batas atas
+
+        Ib = ISPU batas bawah
+
+        Xa = Konsentrasi ambien batas atas (µg/m3)
+
+        Xb = Konsentrasi ambien batas bawah (µg/m3)
+
+        Xx = Konsentrasi ambien nyata hasil pengukuran (µg/m3)
+    """
 )
--- Web/pages/Data_Visualisasi.py
+++ Web/pages/Data_Visualisasi.py
@@ -2,9 +2,16 @@ from sqlalchemy import column
 import streamlit as st
 import plotly_express as px
 import pandas as pd
+from torch import layout
 
 # configuration
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# Judul Halaman
+st.set_page_config(
+    page_title="Regresi dan Klasifikasi Data ISPU",
+    page_icon="📈",
+)
 
 # title of the app
 st.title("Website Visualisasi Prediksi dan Forecasting Data ISPU")
@@ -12,30 +19,106 @@ st.title("Website Visualisasi Prediksi dan Forecasting Data ISPU")
 # Add a sidebar
 st.sidebar.subheader("Visualization Settings")
 
-# Setup file upload
-uploaded_file = st.sidebar.file_uploader(
-                        label="Upload your CSV or Excel file. (200MB max)",
-                         type=['csv', 'xlsx'])
+# Setup selecting csv file
+file_select = st.sidebar.selectbox(
+    label="Pilih file",
+    options=[
+
+                "Prediksi DKI1 PM10", "Prediksi DKI1 SO2", "Prediksi DKI1 CO", "Prediksi DKI1 O3", "Prediksi DKI1 NO2", "Forecasting DKI1",
+                "Prediksi DKI2 PM10", "Prediksi DKI2 SO2", "Prediksi DKI2 CO", "Prediksi DKI2 O3", "Prediksi DKI2 NO2", "Forecasting DKI2",
+                "Prediksi DKI3 PM10", "Prediksi DKI3 SO2", "Prediksi DKI3 CO", "Prediksi DKI3 O3", "Prediksi DKI3 NO2", "Forecasting DKI3",
+                "Prediksi DKI4 PM10", "Prediksi DKI4 SO2", "Prediksi DKI4 CO", "Prediksi DKI4 O3", "Prediksi DKI4 NO2", "Forecasting DKI4",
+                "Prediksi DKI5 PM10", "Prediksi DKI5 SO2", "Prediksi DKI5 CO", "Prediksi DKI5 O3", "Prediksi DKI5 NO2", "Forecasting DKI5",
+            ]
+)
 
 global df
+
+if file_select == "Prediksi DKI1 PM10":
+    uploaded_file = "./files/prediction/DKI1_01_PM10.xlsx"
+elif file_select == "Prediksi DKI1 SO2":
+    uploaded_file = "./files/prediction/DKI1_02_SO2.xlsx"
+elif file_select == "Prediksi DKI1 CO":
+    uploaded_file = "./files/prediction/DKI1_03_CO.xlsx"
+elif file_select == "Prediksi DKI1 O3":
+    uploaded_file = "./files/prediction/DKI1_04_O3.xlsx"
+elif file_select == "Prediksi DKI1 NO2":
+    uploaded_file = "./files/prediction/DKI1_05_NO2.xlsx"
+elif file_select == "Forecasting DKI1":
+    uploaded_file = "./files/forecasting/DKI1_Forecasting.xlsx"
+if file_select == "Prediksi DKI2 PM10":
+    uploaded_file = "./files/prediction/DKI2_01_PM10.xlsx"
+elif file_select == "Prediksi DKI2 SO2":
+    uploaded_file = "./files/prediction/DKI2_02_SO2.xlsx"
+elif file_select == "Prediksi DKI2 CO":
+    uploaded_file = "./files/prediction/DKI2_03_CO.xlsx"
+elif file_select == "Prediksi DKI2 O3":
+    uploaded_file = "./files/prediction/DKI2_04_O3.xlsx"
+elif file_select == "Prediksi DKI2 NO2":
+    uploaded_file = "./files/prediction/DKI2_05_NO2.xlsx"
+elif file_select == "Forecasting DKI2":
+    uploaded_file = "./files/forecasting/DKI2_Forecasting.xlsx"
+if file_select == "Prediksi DKI3 PM10":
+    uploaded_file = "./files/prediction/DKI3_01_PM10.xlsx"
+elif file_select == "Prediksi DKI3 SO2":
+    uploaded_file = "./files/prediction/DKI3_02_SO2.xlsx"
+elif file_select == "Prediksi DKI3 CO":
+    uploaded_file = "./files/prediction/DKI3_03_CO.xlsx"
+elif file_select == "Prediksi DKI3 O3":
+    uploaded_file = "./files/prediction/DKI3_04_O3.xlsx"
+elif file_select == "Prediksi DKI3 NO2":
+    uploaded_file = "./files/prediction/DKI3_05_NO2.xlsx"
+elif file_select == "Forecasting DKI3":
+    uploaded_file = "./files/forecasting/DKI3_Forecasting.xlsx"
+if file_select == "Prediksi DKI4 PM10":
+    uploaded_file = "./files/prediction/DKI4_01_PM10.xlsx"
+elif file_select == "Prediksi DKI4 SO2":
+    uploaded_file = "./files/prediction/DKI4_02_SO2.xlsx"
+elif file_select == "Prediksi DKI4 CO":
+    uploaded_file = "./files/prediction/DKI4_03_CO.xlsx"
+elif file_select == "Prediksi DKI4 O3":
+    uploaded_file = "./files/prediction/DKI4_04_O3.xlsx"
+elif file_select == "Prediksi DKI4 NO2":
+    uploaded_file = "./files/prediction/DKI4_05_NO2.xlsx"
+elif file_select == "Forecasting DKI4":
+    uploaded_file = "./files/forecasting/DKI4_Forecasting.xlsx"
+if file_select == "Prediksi DKI5 PM10":
+    uploaded_file = "./files/prediction/DKI5_01_PM10.xlsx"
+elif file_select == "Prediksi DKI5 SO2":
+    uploaded_file = "./files/prediction/DKI5_02_SO2.xlsx"
+elif file_select == "Prediksi DKI5 CO":
+    uploaded_file = "./files/prediction/DKI5_03_CO.xlsx"
+elif file_select == "Prediksi DKI5 O3":
+    uploaded_file = "./files/prediction/DKI5_04_O3.xlsx"
+elif file_select == "Prediksi DKI5 NO2":
+    uploaded_file = "./files/prediction/DKI5_05_NO2.xlsx"
+elif file_select == "Forecasting DKI5":
+    uploaded_file = "./files/forecasting/DKI5_Forecasting.xlsx"
+
+# # Setup file upload
+# uploaded_file = st.sidebar.file_uploader(
+#                         label="Upload your CSV or Excel file. (200MB max)",
+#                          type=['csv', 'xlsx'])
+
 if uploaded_file is not None:
-    print(uploaded_file)
-    print("hello")
+    # print(uploaded_file)
+    # print("hello")
 
     try:
-        df = pd.read_csv(uploaded_file)
+        df = pd.read_excel(uploaded_file)
     except Exception as e:
         print(e)
-        df = pd.read_excel(uploaded_file)
+        df = pd.read_csv(uploaded_file)
 
 global numeric_columns
 global non_numeric_columns
 try:
     st.write(df)
-    numeric_columns = list(df.select_dtypes(['float', 'int', 'datetime']).columns)
+    date_column = list(df.select_dtypes(['datetime']).columns)
+    numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
     non_numeric_columns = list(df.select_dtypes(['object']).columns)
     non_numeric_columns.append(None)
-    print(non_numeric_columns)
+    # print(non_numeric_columns)
 except Exception as e:
     print(e)
     st.write("Please upload file to the application.")
@@ -46,14 +129,17 @@ chart_select = st.sidebar.selectbox(
     options=['Lineplots']
 )
 
+# Plotting
 if chart_select == 'Lineplots':
     st.sidebar.subheader("Line Plot Settings")
     try:
-        x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
-        y1_values = st.sidebar.selectbox('Y1 axis', options=numeric_columns)
-        y2_values = st.sidebar.selectbox('Y2 axis', options=numeric_columns)
+        x_values = st.sidebar.selectbox('X axis', options=date_column)
+        y_values = st.sidebar.multiselect('Y axis', options=numeric_columns, default=numeric_columns)
+        # y1_values = st.sidebar.selectbox('Y1 axis', options=numeric_columns)
+        # y2_values = st.sidebar.selectbox('Y2 axis', options=numeric_columns)
         color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
-        plot = px.line(data_frame=df, x=x_values, y=[y1_values, y2_values], color=color_value)
+        # plot = px.line(data_frame=df, x=x_values, y=[y1_values, y2_values], color=color_value)
+        plot = px.line(data_frame=df, x=x_values, y=y_values, color=color_value)
         st.plotly_chart(plot)
     except Exception as e:
         print(e)

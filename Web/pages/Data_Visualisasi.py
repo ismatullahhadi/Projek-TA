69b1b10277d4c8aaf4005395c9b0ffaf7886c33a
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2022-07-19 17:03:21 +0700

    Update Data_Visualisasi.py

--- Web/pages/Data_Visualisasi.py
+++ Web/pages/Data_Visualisasi.py
@@ -152,10 +152,10 @@ if chart_select == 'Lineplots':
         )
         # plot = px.line(data_frame=df, x=x_values, y=[y1_values, y2_values], color=color_value)
         plot1 = px.line(data_frame=df, x=x_values, y=y_values, color=color_value)
-        area = {"Tanggal":df["Tanggal"], "Baik":50, "Sedang":100}
+        area = {"Tanggal":df["Tanggal"], "Baik":50, "Sedang":50, "Tidak Sehat":100, "Sangat Tidak Sehat":100, "Berbahaya":100}
         df_area = pd.DataFrame(area)
         st.write(df_area)
-        plot2 = px.area(data_frame=df_area, x="Tanggal", y=["Baik", "Sedang"])
+        plot2 = px.area(data_frame=df_area, x="Tanggal", y=["Baik", "Sedang", "Tidak Sehat", "Sangat Tidak Sehat", "Berbahaya"])
         plot3 = go.Figure(data=plot1.data + plot2.data)
         st.plotly_chart(plot2)
     except Exception as e:

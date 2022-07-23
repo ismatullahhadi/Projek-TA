4c708d5338547ee32d3da323eb17f127c01f9f60
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2022-07-23 11:36:47 +0700

    Update 2_📄_Credits.py

--- Web/pages/2_📄_Credits.py
+++ Web/pages/2_📄_Credits.py
@@ -22,14 +22,3 @@ st.markdown(
         Website ini dibuat menggunakan streamlit.
     """
 )
-
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)

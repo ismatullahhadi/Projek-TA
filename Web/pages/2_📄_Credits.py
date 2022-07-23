5ea4527bd1905acc7ec4c94c72bf8d49df3137f9
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2022-07-23 11:32:50 +0700

    Update 2_📄_Credits.py

--- Web/pages/2_📄_Credits.py
+++ Web/pages/2_📄_Credits.py
@@ -2,6 +2,17 @@ import streamlit as st
 # from PIL import Image
 # from pathlib import Path
 
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.set_page_config(
     page_title="Credits",
     page_icon="📄",

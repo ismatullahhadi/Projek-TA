6df1adfee88fb774d93c77ceea90c0e2dfd09568
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2022-07-23 11:36:25 +0700

    Update 2_📄_Credits.py

--- Web/pages/2_📄_Credits.py
+++ Web/pages/2_📄_Credits.py
@@ -7,17 +7,6 @@ st.set_page_config(
     page_icon="📄",
 )
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 st.markdown(
     """
         ### Website Projek TA ini dibuat oleh:\n
@@ -33,3 +22,14 @@ st.markdown(
         Website ini dibuat menggunakan streamlit.
     """
 )
+
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)

7bdecfec9caf6967853a760795d2af234fef630d
Ismatullah Hadi <73352495+ismatullahhadi@users.noreply.github.com>
2025-07-16 15:57:49 +0700

    Update 1_📈_Data_Visualisasi.py

--- Web/pages/1_📈_Data_Visualisasi.py
+++ Web/pages/1_📈_Data_Visualisasi.py
@@ -111,7 +111,7 @@ if uploaded_file is not None:
         df = pd.read_excel(uploaded_file)
     except Exception as e:
         print(e)
-        df = pd.read_csv(uploaded_file, encoding='utf-8', errors='replace')
+        df = pd.read_csv(uploaded_file, encoding='utf-8', engine='python')
 
 global numeric_columns
 global non_numeric_columns
